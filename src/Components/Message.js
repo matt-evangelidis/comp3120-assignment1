e355e82e94652cc6ca66ed75c48757517913aa16
Matthew Evangelidis <54091051+Notintheslightest@users.noreply.github.com>
2020-09-14 11:30:25 +1000

    Installed Express, ran npm init on the server folder, added server/package.json

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Message = ({user, message}) => {
     
-    const userURL = "/users/" + user.name;
+    const userURL = "/users/" + user.id;
     console.log(user)
     
     return (
     <div>
-        <div><img src = {user.image} alt = {user.name}/></div>
-        <p><a className = "Message" href = {userURL}>@{user.name}</a>: {message.timestamp} {message.content}</p>
+        <div><img src = {user.avatar} alt = {user.id}/></div>
+        <p><a className = "Message" href = {userURL}>@{user.id}</a>: {message.timestamp} {message.content}</p>
     </div>
     )
 }
