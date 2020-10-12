7796572dbe4662ffcff8542120716b8dbc14d281
DESKTOP-DAB50SO\matth <matthew.evangelidis@students.mq.edu.au>
2020-10-12 13:46:25 +1100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,13 +13,13 @@ const loginRouter = require('./controllers/login')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(apiRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/posts', postsRouter)
 app.use('/api/login', loginRouter)
 
-app.use(express.static('build'))
-// app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')))
+app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')))
 
 const PORT = process.env.PORT || 3001
 console.log(PORT)
